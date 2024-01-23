df1df3d967510469a5dc1348b48cc6d2a487df85
Renata Dal Bello Sanchez <reenadbsanchez@gmail.com>
2024-01-23 17:16:01 -0300

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+
+    let [number, setNumber] = useState(0)
+    let [warning, setWarning]= useState('')
+
+    function minus(){
+        if(number > 0){
+            setNumber(number - 1)
+        }else if(number == 0){
+            setWarning(warning = 'Você não pode contar abaixo de 0')
+        }
+    }
+
+    function plus(){
+        setNumber(number + 1)
+        setWarning(warning = '')
+    }
+
+    function reset(){
+        setNumber(number = 0)
+        setWarning(warning = '')
+    }
+
+
+    return <section>
+        <p class="warning">{warning}</p>
+        <h1>Contador</h1>
+        <p>{number}</p>
+        <div class="button-block">
+            <button onClick={minus}>-</button>
+            <button onClick={plus}>+</button>
+        </div>
+        <br/>
+        <br/>
+        <button onClick={reset} class="reset-button">reset</button>
+    </section>
 }
-
-export default App;
